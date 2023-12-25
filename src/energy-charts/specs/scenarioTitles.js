43f052f397427f07bac9e7d19abed8dfbb818eb2
AZPS <github@londonanalytics.info>
2023-12-25 09:56:17 +0000

    remove redundant files

--- src/energy-charts/specs/scenarioTitles.js
+++
@@ -1,8 +0,0 @@
-const scenarioTitles = {
-  "Scenario 1": "Scenario One",
-  "Scenario 2": "Scenario Two",
-  "Scenario 3": "Scenario Three",
-  "Scenario 4": "Scenario Four"
-};
-
-export default scenarioTitles;
